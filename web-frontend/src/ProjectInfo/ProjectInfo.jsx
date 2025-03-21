11b0546f24a1cbace1af61eccf80d5e334e851f0
alexcrist <alexecrist@gmail.com>
2025-03-21 13:32:19 -0700

    Add suggested citation

--- web-frontend/src/ProjectInfo/ProjectInfo.jsx
+++ web-frontend/src/ProjectInfo/ProjectInfo.jsx
@@ -1,19 +1,10 @@
 import { Attributions } from "../Attributions/Attributions";
 import CardSection from "../CardSection/CardSection";
+import styles from "./ProjectInfo.module.css";
 
 const ProjectInfo = () => {
     return (
         <>
-            {/* <div className={styles.header}>
-                <div className={styles.headerTexts}>
-                    <h1 className={styles.headerText}>
-                        US State Health AI Legislation
-                    </h1>
-                    <h3 className={styles.subheaderText}>
-                        Insurance, Infrastructure, and Privacy Laws
-                    </h3>
-                </div>
-            </div> */}
             <CardSection
                 title="About"
                 descriptions={[
@@ -36,6 +27,23 @@ const ProjectInfo = () => {
                     </>,
                 ]}
             />
+            <CardSection title="Suggested Citation">
+                <div className={styles.citationTitle}>
+                    To cite the US State Health AI Legislation Map, please use
+                    the following citation:
+                </div>
+                <div className={styles.citation}>
+                    Ian Stevens, Alden Blatter, Erin Williams, Yael Bensoussan,
+                    Bridge to AI Voice Consortium, Jean-Christophe
+                    Bélisle-Pipon, Vardit Ravitsky. “US States Health AI
+                    Legislation Dataset.” (2025). Hastings on the Hill.
+                    Retrieved Month, Day, Year, from{" "}
+                    <a href="med-ai-laws.thehastingcenter.org" target="_">
+                        med-ai-laws.thehastingcenter.org
+                    </a>
+                    .
+                </div>
+            </CardSection>
             <CardSection title="Attributions" descriptions={[]}>
                 <Attributions />
             </CardSection>
