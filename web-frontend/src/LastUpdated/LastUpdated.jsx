9d03436353fd419107386415581997abe88c9c7e
alexcrist <alexecrist@gmail.com>
2025-09-26 16:25:51 -0700

    Add last updated card

---
+++ web-frontend/src/LastUpdated/LastUpdated.jsx
@@ -0,0 +1,8 @@
+import Card from '../Card/Card';
+import styles from './LastUpdated.module.css';
+
+const LastUpdated = () => {
+  return <Card className={styles.lastUpdated}>Last updated March 2025</Card>;
+};
+
+export default LastUpdated;
--- web-frontend/src/map/Map/Map.jsx
+++ web-frontend/src/map/Map/Map.jsx
@@ -1,66 +1,66 @@
-import classNames from "classnames";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { MAP_CONTAINER_ID, USA_BBOX } from "../../constants";
-import Legend from "../../Legend/Legend";
-import mainSlice from "../../mainSlice";
-import StarsBackground from "../../StarsBackground/StarsBackground";
-import { getStateAtPoint } from "../getStateAtPoint";
-import { useAddMapClickListener, useFlyToBbox, useInitMap } from "../map";
-import { useFlyToState } from "../useFlyToState";
-import { useShowHoveredState } from "../useShowHoveredState";
-import styles from "./Map.module.css";
+import classNames from 'classnames';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { MAP_CONTAINER_ID, USA_BBOX } from '../../constants';
+import LastUpdated from '../../LastUpdated/LastUpdated';
+import Legend from '../../Legend/Legend';
+import mainSlice from '../../mainSlice';
+import StarsBackground from '../../StarsBackground/StarsBackground';
+import { getStateAtPoint } from '../getStateAtPoint';
+import { useAddMapClickListener, useFlyToBbox, useInitMap } from '../map';
+import { useFlyToState } from '../useFlyToState';
+import { useShowHoveredState } from '../useShowHoveredState';
+import styles from './Map.module.css';
 
 let hasFlown = false;
 
 const Map = () => {
-    // Initialize the map
-    const isMapInitialized = useSelector(
-        (state) => state.main.isMapInitialized,
-    );
-    useInitMap();
+  // Initialize the map
+  const isMapInitialized = useSelector((state) => state.main.isMapInitialized);
+  useInitMap();
 
-    // Fly to target on load
-    const flyToBbox = useFlyToBbox();
-    useEffect(() => {
-        if (isMapInitialized && !hasFlown) {
-            hasFlown = true;
-            flyToBbox(USA_BBOX, { duration: 2000 });
-        }
-    }, [flyToBbox, isMapInitialized]);
+  // Fly to target on load
+  const flyToBbox = useFlyToBbox();
+  useEffect(() => {
+    if (isMapInitialized && !hasFlown) {
+      hasFlown = true;
+      flyToBbox(USA_BBOX, { duration: 2000 });
+    }
+  }, [flyToBbox, isMapInitialized]);
 
-    // Allow state selection
-    const addMapClickListener = useAddMapClickListener();
-    const flyToState = useFlyToState();
-    const dispatch = useDispatch();
-    useEffect(() => {
-        return addMapClickListener(async (event) => {
-            const lon = event.lngLat.lng;
-            const lat = event.lngLat.lat;
-            const state = await getStateAtPoint(lon, lat);
-            if (state) {
-                dispatch(mainSlice.actions.setFocusedState(state));
-                flyToState(state);
-            }
-        });
-    }, [addMapClickListener, dispatch, flyToState]);
+  // Allow state selection
+  const addMapClickListener = useAddMapClickListener();
+  const flyToState = useFlyToState();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    return addMapClickListener(async (event) => {
+      const lon = event.lngLat.lng;
+      const lat = event.lngLat.lat;
+      const state = await getStateAtPoint(lon, lat);
+      if (state) {
+        dispatch(mainSlice.actions.setFocusedState(state));
+        flyToState(state);
+      }
+    });
+  }, [addMapClickListener, dispatch, flyToState]);
 
-    // On map hover, show info pop-up
-    const hoveredState = useShowHoveredState();
+  // On map hover, show info pop-up
+  const hoveredState = useShowHoveredState();
 
-    return (
-        <div
-            className={classNames(styles.container, {
-                [styles.isMapInitialized]: isMapInitialized,
-            })}
-        >
-            <StarsBackground />
-            <div className={styles.loading}>Loading...</div>
-            <div className={styles.map} id={MAP_CONTAINER_ID} />
-            {hoveredState}
-            <Legend />
-        </div>
-    );
+  return (
+    <div
+      className={classNames(styles.container, {
+        [styles.isMapInitialized]: isMapInitialized,
+      })}
+    >
+      <StarsBackground />
+      <div className={styles.loading}>Loading...</div>
+      <div className={styles.map} id={MAP_CONTAINER_ID} />
+      {hoveredState}
+      <Legend />
+      <LastUpdated />
+    </div>
+  );
 };
 
 export default Map;
